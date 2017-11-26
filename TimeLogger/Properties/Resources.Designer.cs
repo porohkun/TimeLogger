6169d354b59d3aec5440a531e4d275374cb319f7
Poroh <meta.poroh.kun@gmail.com>
2017-11-26 18:55:03 +0200

    fix crash on get task

--- TimeLogger/Properties/Resources.Designer.cs
+++ TimeLogger/Properties/Resources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace TimeLogger.Properties
 {
+    using System;
 
 
     /// <summary>
@@ -19,7 +20,7 @@ namespace TimeLogger.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
@@ -42,7 +43,7 @@ namespace TimeLogger.Properties
         {
             get
             {
-                if ((resourceMan == null))
+                if (object.ReferenceEquals(resourceMan, null))
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TimeLogger.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
