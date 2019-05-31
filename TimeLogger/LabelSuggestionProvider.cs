53edd0bafae456295e6c2ae696c7e85e303f427c
Poroh <meta.poroh.kun@gmail.com>
2019-06-01 00:37:09 +0300

    added quicoli's WPF-AutoComplete-TextBox as submodule

--- TimeLogger/LabelSuggestionProvider.cs
+++ TimeLogger/LabelSuggestionProvider.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using WpfControls.Editors;
+using AutoCompleteTextBox.Editors;
 
 namespace TimeLogger
 {
--- TimeLogger/Windows/MainWindow.xaml.cs
+++ TimeLogger/Windows/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfControls.Editors;
+using AutoCompleteTextBox.Editors;
 using System.ComponentModel;
 
 namespace TimeLogger
